bfcad35248a58b53d222b593bcd25799a4929b29
yao <september9m24@gmail.ocm>
2019-10-02 17:28:35 +0800

    test1

--- Lab01/bonus/divider.v
+++ Lab01/bonus/divider.v
@@ -5,15 +5,22 @@ module divider(
 );
 
   reg [26:0] cnt;
-
+  reg xxx;
   always@(posedge clk or posedge rst) begin
     if (rst) begin
+      xxx <= 1'b0;
       cnt <= 27'd0;
       clk_div <= 27'b0;
     end
     else begin
-
-      if (cnt == 125000000 - 1) begin
+      if (start && !sw && !xxx)begin
+          cnt <= 0;
+          xxx <= 1;
+      end
+      else if(!start || sw) begin
+        xxx <= 0;
+      end
+      else if (cnt == 125000000 - 1) begin
 			cnt <= 27'd0;
 			clk_div <= 'b1;
 		end

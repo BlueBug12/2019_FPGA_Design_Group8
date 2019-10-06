bffc09fa01c49a23108fad8b478f572627f89511
BlueBug12 <benson.liu.himself@gmail.com>
2019-10-06 13:52:39 +0800

    upload file

--- Lab01/bonus/RGB_LED.v
+++ Lab01/bonus/RGB_LED.v
@@ -12,50 +12,30 @@ module RGB_LED(
 	output reg[3:0]led
 	);
 reg [6:0]counter;
+reg [3:0]sub_counter;
 reg [3:0]timer;
 reg [3:0]t1;
 reg [3:0]t2;
 reg [3:0]t3;
-parameter NORMAL = 2'b00;
+reg [6:0]a;
+reg [6:0]b;
+reg [6:0]c;
+reg [6:0]d;
+reg [6:0]e;
+reg [6:0]f;
+
 parameter T1 = 2'b01;
 parameter T2 = 2'b10;
 parameter T3 = 2'b11;
 
-always@(negedge start)begin
-	case(sw)
-		T1:begin
-			t1<=timer;
-			timer<=4'b0;
-		end
-		
-		T2:begin
-			t2<=timer;
-			timer<=4'b0;
-		end
-		
-		T3:begin
-			t3<=timer;
-			timer<=4'b0;
-		end
-		
-		default:begin
-		
-		end
-	endcase
+always@(*)begin
+    a=t3;
+    b=a+t2;
+    c=b+t1;
+    d=c+t3;
+    e=d+t2;
+    f=e+t1;
 end
-always@(posedge clk)begin
-	if(start)begin
-		timer<=timer+4'b1;
-		led[0]<=timer[0];
-		led[1]<=timer[1];
-		led[2]<=timer[2];
-		led[3]<=timer[3];
-	end
-	else 
-		led<=4'b0;
-end
-
-
 
 always@(posedge clk or posedge rst)begin
     if(rst)begin
@@ -66,44 +46,100 @@ always@(posedge clk or posedge rst)begin
         led5_b<=0;
         led5_g<=0;
         led5_r<=0;
-		t1<=4'd1;
+        t1<=4'd1;
 		t2<=4'd5;
 		t3<=4'd1;
+	    led<=4'b0;
+	    sub_counter<=4'b1;
+		
     end
     else begin
-        counter<=counter+7'b1;
-		
+        if(start)begin
+            timer<=timer+4'b1;
+            if(sw) begin
+                led<=timer;
+            end
+            else begin
+                led<=4'b0;
+            end
+            case(sw)
+                2'b01:begin
+                    t1<=timer;
+                end
+                
+                T2:begin
+                    t2<=timer;
+                end
+                
+                T3:begin
+                    t3<=timer;
+                end
+                
+                default:begin
+                    
+                end
+            endcase
+        end
+        else begin
+            timer<=4'b0;
+            led<=sub_counter;
+        end
+        
+        
         case(counter)
-            7'd0:begin
+            7'd0:begin//RR
                 led4_g<=1'b0;
                 led4_b<=1'b0;
                 led5_g<=1'b0;
                 led5_b<=1'b0;
                 led4_r<=1'b1;
                 led5_r<=1'b1;
+                counter<=counter+7'b1;
+                sub_counter<=4'b1;
             end
-            t3:begin
+            a:begin//GR
                 led4_r<=1'b0;
                 led4_g<=1'b1;
+                counter<=counter+7'b1;
+                sub_counter<=4'b1;
             end
             
-            t3+t2:begin
+            b:begin//YR
                 led4_r<=1'b1;
+                counter<=counter+7'b1;
+                sub_counter<=4'b1;
             end
             
-            t3+t2+t1:begin
+            c:begin//RR
                 led4_g<=1'b0;
+                counter<=counter+7'b1;
+                sub_counter<=4'b1;
             end
             
-            t3+t2+t1+t3:begin
+            d:begin//RG
                 led5_r<=1'b0;
                 led5_g<=1'b1;
+                counter<=counter+7'b1;
+                sub_counter<=4'b1;
             end
             
-            t3+t2+t1+t3+t2:begin
+            e:begin//RY
                 led5_r<=1'b1;
-                counter<=7'b0;
+                counter<=counter+7'b1;
+                sub_counter<=4'b1;
             end
+			
+			f:begin
+				counter<=7'b0;
+				sub_counter<=4'b0;
+            
+			end
+			
+			default:begin
+			    counter<=counter+7'b1;
+                sub_counter<=sub_counter+4'b1;
+			end
+			
             
         endcase
 	end
--- Lab01/bonus/divider.v
+++ Lab01/bonus/divider.v
@@ -1,25 +1,42 @@
 module divider(
   input clk,//125MHz
   input rst,
+  input start,
   output reg clk_div
 );
-
+  reg recover;
   reg [26:0] cnt;
 
   always@(posedge clk or posedge rst) begin
     if (rst) begin
       cnt <= 27'd0;
-      clk_div <= 27'b0;
+      clk_div <= 1'b0;
+      recover <= 1'b0;
     end
     else begin
-
       if (cnt == 125000000 - 1) begin
 			cnt <= 27'd0;
-			clk_div <= 'b1;
+			clk_div <= 1'b1;
 		end
-      else begin
-			cnt <= cnt + 1;
-			clk_div <= 27'b0;
+      else begin      
+			if(!recover) begin
+			     if(start) begin
+			         clk_div<=1'b1;
+			         recover<=1'b1;
+			         cnt<=27'b0;
+			     end
+			     else begin
+			         cnt <= cnt + 27'b1;
+			         clk_div <= 1'b0;
+			     end
+			     
+			end
+			else begin
+			     cnt <= cnt + 27'b1;
+			     clk_div <= 1'b0;
+			
+			end
+			 
 		end
     end
   end

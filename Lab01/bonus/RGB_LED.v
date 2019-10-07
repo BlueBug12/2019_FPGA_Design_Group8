6d0bd15cc15375160a9c147fc07f869416963332
BlueBug12 <benson.liu.himself@gmail.com>
2019-10-07 10:41:01 +0800

    correct version. 1.Fix the bug of delay sub_clock 2.Add the mode displayer

--- Lab01/bonus/RGB_LED.v
+++ Lab01/bonus/RGB_LED.v
@@ -23,7 +23,8 @@ reg [6:0]c;
 reg [6:0]d;
 reg [6:0]e;
 reg [6:0]f;
-
+reg flag;
+parameter NORMAL = 2'b00;
 parameter T1 = 2'b01;
 parameter T2 = 2'b10;
 parameter T3 = 2'b11;
@@ -42,10 +43,10 @@ always@(posedge clk or posedge rst)begin
         counter<=7'b0;
         led4_b<=0;
         led4_g<=0;
-        led4_r<=0;
+        led4_r<=1;
         led5_b<=0;
         led5_g<=0;
-        led5_r<=0;
+        led5_r<=1;
         t1<=4'd1;
 		t2<=4'd5;
 		t3<=4'd1;
@@ -56,92 +57,124 @@ always@(posedge clk or posedge rst)begin
     else begin
         if(start)begin
             timer<=timer+4'b1;
-            if(sw) begin
-                led<=timer;
-            end
-            else begin
-                led<=4'b0;
-            end
             case(sw)
-                2'b01:begin
+                T1:begin
                     t1<=timer;
+                    led<=timer;
                 end
                 
                 T2:begin
                     t2<=timer;
+                    led<=timer;
                 end
                 
                 T3:begin
                     t3<=timer;
+                    led<=timer;
                 end
                 
                 default:begin
-                    
+                    led<=4'b0;
                 end
             endcase
         end
         else begin
             timer<=4'b0;
-            led<=sub_counter;
-        end
+            case(sw)
+                NORMAL:begin
+                     case(counter)
+                        a:begin//GR
+                            led4_r<=1'b0;
+                            led4_g<=1'b1;
+                            counter<=counter+7'b1;
+                            sub_counter<=4'b1;
+                            led<=4'b1;
+                        end
+                        
+                        b:begin//YR
+                            led4_r<=1'b1;
+                            counter<=counter+7'b1;
+                            sub_counter<=4'b1;
+                            led<=4'b1;
+                        end
+                        
+                        c:begin//RR
+                            led4_g<=1'b0;
+                            counter<=counter+7'b1;
+                            sub_counter<=4'b1;
+                            led<=4'b1;
+                        end
+                        
+                        d:begin//RG
+                            led5_r<=1'b0;
+                            led5_g<=1'b1;
+                            counter<=counter+7'b1;
+                            sub_counter<=4'b1;
+                            led<=4'b1;
+                        end
+                        
+                        e:begin//RY
+                            led5_r<=1'b1;
+                            counter<=counter+7'b1;
+                            sub_counter<=4'b1;
+                            led<=4'b1;
+                        end
+                        
+                        f:begin//RR
+                            led4_g<=1'b0;
+                            led4_b<=1'b0;
+                            led5_g<=1'b0;
+                            led5_b<=1'b0;
+                            led4_r<=1'b1;
+                            led5_r<=1'b1;
+                            counter<=7'b1;
+                            sub_counter<=4'b1;
+                            led<=4'b1;
+                        
+                        end
+                        
+                        default:begin
+                            counter<=counter+7'b1;
+                            sub_counter<=sub_counter+4'b1;
+                            led<=sub_counter+4'b1;
+                        end
+                    endcase
+              end
+              T1:begin//yellow
+                led4_b<=0;
+                led4_g<=1;
+                led4_r<=1;
+                led5_b<=0;
+                led5_g<=1;
+                led5_r<=1;
+              end
+              
+              T2:begin//green
+                led4_b<=0;
+                led4_g<=1;
+                led4_r<=0;
+                led5_b<=0;
+                led5_g<=1;
+                led5_r<=0;
+              end
+              
+              T3:begin//red
+                led4_b<=0;
+                led4_g<=0;
+                led4_r<=1;
+                led5_b<=0;
+                led5_g<=0;
+                led5_r<=1;
+              end
+              
+              default:begin
+                 led<=4'b0;
+              end
+           endcase
+        end//outer else
         
         
-        case(counter)
-            7'd0:begin//RR
-                led4_g<=1'b0;
-                led4_b<=1'b0;
-                led5_g<=1'b0;
-                led5_b<=1'b0;
-                led4_r<=1'b1;
-                led5_r<=1'b1;
-                counter<=counter+7'b1;
-                sub_counter<=4'b1;
-            end
-            a:begin//GR
-                led4_r<=1'b0;
-                led4_g<=1'b1;
-                counter<=counter+7'b1;
-                sub_counter<=4'b1;
-            end
-            
-            b:begin//YR
-                led4_r<=1'b1;
-                counter<=counter+7'b1;
-                sub_counter<=4'b1;
-            end
-            
-            c:begin//RR
-                led4_g<=1'b0;
-                counter<=counter+7'b1;
-                sub_counter<=4'b1;
-            end
-            
-            d:begin//RG
-                led5_r<=1'b0;
-                led5_g<=1'b1;
-                counter<=counter+7'b1;
-                sub_counter<=4'b1;
-            end
-            
-            e:begin//RY
-                led5_r<=1'b1;
-                counter<=counter+7'b1;
-                sub_counter<=4'b1;
-            end
-			
-			f:begin
-				counter<=7'b0;
-				sub_counter<=4'b0;
-            
-			end
-			
-			default:begin
-			    counter<=counter+7'b1;
-                sub_counter<=sub_counter+4'b1;
-			end
-			
-            
-        endcase
+       
 	end
 end
 endmodule

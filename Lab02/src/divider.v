a52cc632698183ce4c6097c7a14cb19797a2913e
BlueBug12 <benson.liu.himself@gmail.com>
2019-10-08 08:26:02 +0800

    upload correct version

--- Lab02/src/divider.v
+++ Lab02/src/divider.v
@@ -9,17 +9,17 @@ module divider(
   always@(posedge clk or posedge rst) begin
     if (rst) begin
       cnt <= 28'd0;
-      clk_div <= 1'b0;
+      clk_div <= 28'b0;
     end
     else begin
 
       if (cnt == 250000000 - 1) begin
 			cnt <= 28'd0;
-			clk_div <= 1'b1;
+			clk_div <= 'b1;
 		end
       else begin
 			cnt <= cnt + 1;
-			clk_div <= 1'b0;
+			clk_div <= 28'b0;
 		end
     end
   end

c5f538950e2d2626f6b88240dfb0dd43a518d3c3
yao <september9m24@gmail.ocm>
2019-10-14 14:24:47 +0800

    upload image

--- Lab02/bonus/IP/clk_divider/src/clk_divider.v
+++ Lab02/bonus/IP/clk_divider/src/clk_divider.v
@@ -1,11 +1,10 @@
 module clk_divider(
   input clk,//100MHz
   input rst,
- // output reg clk_color,
   output reg clk_breath
 );
 
-    parameter max_breath = 2560000;
+    parameter max_breath = 640000;
     reg [31:0] counter1;
     reg [31:0] counter2;
 
@@ -13,7 +12,6 @@ module clk_divider(
         if (rst) begin
             counter1 <= 0;
             counter2 <= 0;
-         //   clk_color <= 0;
             clk_breath <= 0;
         end
         else begin
@@ -24,20 +22,6 @@ module clk_divider(
             else if (counter1 == max_breath - 1) begin
                 counter1 <= 0;
                 clk_breath <= 0;
-                /*
-                if(counter2 == 160/2 - 1) begin
-                    counter2 <= counter2 + 1;
-                    clk_color <= 1;
-                end
-                else if (counter2 == 160 - 1) begin
-                    counter2 <= 0;
-                    clk_color <= 0;
-                end
-                else begin
-                    counter2 <= counter2 + 1;
-                    clk_color <= clk_color;
-                end 
-                */
             end
             else begin
                 counter1 <= counter1 + 1;
